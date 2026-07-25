005aeb455c652eef0fbf3d3b6ce680417aa735ac
romitoorazio <romitoorazio@gmail.com>
2026-07-25 21:15:00 +0200

    Confine external strategy payload lint exception

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,11 @@ import nextTs from "eslint-config-next/typescript";
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["lib/strategy-runtime.ts"],
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
