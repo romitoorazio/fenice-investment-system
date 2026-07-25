2d06339444fb95e3100d95b4584852572482b4c5
romitoorazio <romitoorazio@gmail.com>
2026-07-25 18:59:26 +0200

    Add fundamental research page

---
+++ app/research/page.tsx
@@ -0,0 +1,8 @@
+import ResearchEngine from "@/components/ResearchEngine";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function ResearchPage() {
+  return <ResearchEngine />;
+}
