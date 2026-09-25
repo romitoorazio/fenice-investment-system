f1262291be0c24b87412bbc29680705a16b74771
romitoorazio <romitoorazio@gmail.com>
2026-09-25 22:17:39 +0200

    Expose Fenice V7 institutional dossier page

---
+++ app/dossier/page.tsx
@@ -0,0 +1,8 @@
+import InstitutionalDossier from "@/components/InstitutionalDossier";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function DossierPage() {
+  return <InstitutionalDossier />;
+}
